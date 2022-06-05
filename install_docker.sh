309c955a2e5a1cd5026c44614448af0df18cd69c
dangvinhnd99 <84520620+dangvinhnd99@users.noreply.github.com>
2022-06-05 22:27:47 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,4 +2,4 @@
 sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
-sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker ec2-user  #Thêm người dùng ec2 vào nhóm docker để bạn có thể thực thi các lệnh Docker mà không cần sử dụng Sudo.
