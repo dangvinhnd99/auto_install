8e9732d3978c7b942b37e517a64be73fd9f54b16
dangvinhnd99 <84520620+dangvinhnd99@users.noreply.github.com>
2022-06-05 23:24:10 +0700

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@
 yum update -y
 yum install git -y
 yum install java-1.8.0-openjdk -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo #cài jenkins
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key #import key của repo về
 yum install -y jenkins
-systemctl start jenkins
+systemctl start jenkins   #start jenkins
